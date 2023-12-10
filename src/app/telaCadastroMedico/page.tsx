d36477eb04803ccac9e35a277dac1c3461b0a492
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-12-10 18:21:08 -0300

    ajustando formulários de cadastro

--- src/app/telaCadastroMedico/page.tsx
+++ src/app/telaCadastroMedico/page.tsx
@@ -2,45 +2,84 @@ import React from "react";
 import { Button, TextField, Typography, Link, Container } from "@mui/material";
 import NavbarCadastro from "@/components/NavbarCadastro";
 
-export default function telaCadastroMedico() {
+export default function TelaCadastroMedico() {
   return (
     <Container component="main" maxWidth="xs">
-      <NavbarCadastro />
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "1rem" }}>
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h5" gutterBottom>
           MedLine
         </Typography>
+        <div style={{marginTop: "3rem"}}/>
+        <NavbarCadastro />
+        <div style={{marginTop: "3rem"}}/>
+        <h1>Cadastrar como Médico</h1>
         
         <form style={{ width: "100%", maxWidth: 400, marginTop: "1rem" }}>
           <TextField
             variant="outlined"
             margin="normal"
             fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
+            id="fullName"
+            label="Nome completo"
+            name="fullName"
+            autoComplete="name"
             autoFocus
           />
           <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          name="documentoMedico"
-          label="documentoMedico"
-          type="documentoMedico"
-          id="documentoMedico">
-            Documento do Médico
-          </TextField>
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="username"
+            label="Nome de usuário"
+            name="username"
+            autoComplete="username"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="cpf"
+            label="CPF"
+            name="cpf"
+            autoComplete="off"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="documentoMedico"
+            label="Documento do Médico"
+            name="documentoMedico"
+            autoComplete="off"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+          />
           <TextField
             variant="outlined"
             margin="normal"
             fullWidth
             name="password"
-            label="Password"
+            label="Senha"
             type="password"
             id="password"
-            autoComplete="current-password"
+            autoComplete="new-password"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="confirmPassword"
+            label="Confirmar senha"
+            type="password"
+            id="confirmPassword"
+            autoComplete="new-password"
           />
           <Button
             type="submit"
@@ -49,16 +88,9 @@ export default function telaCadastroMedico() {
             color="primary"
             style={{ marginTop: "2rem" }}
           >
-            Entrar
+            Cadastrar
           </Button>
         </form>
-
-        <Typography variant="body2" align="center" style={{ marginTop: "2rem" }}>
-          Novo por aqui?{" "}
-          <Link href="/cadastro" variant="body2">
-            Cadastre-se clicando aqui
-          </Link>
-        </Typography>
       </div>
     </Container>
   );
--- src/app/telaCadastroPaciente/page.tsx
+++ src/app/telaCadastroPaciente/page.tsx
@@ -5,33 +5,72 @@ import NavbarCadastro from "@/components/NavbarCadastro";
 export default function TelaCadastroPaciente() {
   return (
     <Container component="main" maxWidth="xs">
-      <NavbarCadastro />
-
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "1rem" }}>
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h5" gutterBottom>
           MedLine
         </Typography>
+        <div style={{marginTop: "3rem"}}/>
+        <NavbarCadastro/>
+        <div style={{marginTop: "3rem"}}/>
+        <h1>Cadastrar como Paciente</h1>
 
         <form style={{ width: "100%", maxWidth: 400, marginTop: "1rem" }}>
           <TextField
             variant="outlined"
             margin="normal"
             fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
+            id="fullName"
+            label="Nome completo"
+            name="fullName"
+            autoComplete="name"
             autoFocus
           />
           <TextField
             variant="outlined"
             margin="normal"
             fullWidth
+            id="username"
+            label="Nome de usuário"
+            name="username"
+            autoComplete="username"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="cpf"
+            label="CPF"
+            name="cpf"
+            autoComplete="off"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
             name="password"
-            label="Password"
+            label="Senha"
             type="password"
             id="password"
-            autoComplete="current-password"
+            autoComplete="new-password"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="confirmPassword"
+            label="Confirmar senha"
+            type="password"
+            id="confirmPassword"
+            autoComplete="new-password"
           />
           <Button
             type="submit"
@@ -40,16 +79,9 @@ export default function TelaCadastroPaciente() {
             color="primary"
             style={{ marginTop: "2rem" }}
           >
-            Entrar
+            Cadastrar
           </Button>
         </form>
-
-        <Typography variant="body2" align="center" style={{ marginTop: "2rem" }}>
-          Novo por aqui?{" "}
-          <Link href="/cadastro" variant="body2">
-            Cadastre-se clicando aqui
-          </Link>
-        </Typography>
       </div>
     </Container>
   );
