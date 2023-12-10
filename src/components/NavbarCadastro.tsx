9e97dc3d266a22b2979cb8ae3f4907452d97a493
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-12-10 18:31:12 -0300

    estilizando os botões

--- src/components/NavbarCadastro.tsx
+++ src/components/NavbarCadastro.tsx
@@ -1,24 +1,28 @@
-// components/NavbarCadastro.tsx
 import React from "react";
 import { Button, Toolbar, AppBar } from "@mui/material";
 import NextLink from "next/link";
+
+const buttonStyle = {
+  borderRadius: 20,
+  margin: "0 8px",
+};
 
 const NavbarCadastro: React.FC = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <NextLink href="/telaCadastroPaciente" passHref>
-          <Button component="a" color="inherit">
+          <Button component="a" color="inherit" style={buttonStyle}>
             Cadastrar como Paciente
           </Button>
         </NextLink>
         <NextLink href="/telaCadastroMedico" passHref>
-          <Button component="a" color="inherit">
+          <Button component="a" color="inherit" style={buttonStyle}>
             Cadastrar como Médico
           </Button>
         </NextLink>
         <NextLink href="/telaCadastroRecepcionista" passHref>
-          <Button component="a" color="inherit">
+          <Button component="a" color="inherit" style={buttonStyle}>
             Cadastrar como Recepcionista
           </Button>
         </NextLink>
