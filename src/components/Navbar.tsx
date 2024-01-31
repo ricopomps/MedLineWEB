aea02aa23d99cb6e6ab561d96721863243c4f9e3
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2024-01-31 18:48:16 -0300

    definindo rota  p/ home no LOGO da Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/home"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
