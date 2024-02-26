d958e91693e16cd61e28d878f0a119d01d6cb7b1
gxvl <geraldo.goncalves@ufrpe.br>
2024-02-25 23:48:23 -0300

    visual

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <UserProvider>
           {children}
-          <Rodape />
           <ToastContainer />
         </UserProvider>
       </body>
